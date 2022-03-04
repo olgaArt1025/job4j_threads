ba8f3c929042ff56430b17aeeb7cbea71c86dc01
Olga_ <olga_art1025@mail.ru>
2022-03-05 01:30:32 +0500

    3. Прерывание нити [#1019]

---
+++ src/test/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,28 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        char[] process = {'\\', '|', '/'};
+        int count = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            if (count == process.length) {
+                count = 0;
+            }
+            System.out.print("\r load: " + process[count]);
+            count++;
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(1000);
+        progress.interrupt();
+    }
+}
