0df7f5f1a9b59d798589c1d2f11175d28909fcd7
Olga_ <olga_art1025@mail.ru>
2022-03-06 15:39:26 +0500

    4. Скачивание файла с ограничением. [#144271]

---
+++ src/test/java/ru/job4j/net/Wget.java
@@ -0,0 +1,51 @@
+package ru.job4j.net;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+    private final String file;
+
+    public Wget(String url, int speed, String file) {
+        this.url = url;
+        this.speed = speed;
+        this.file = file;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream(file)) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            int downloadSpeed = 0;
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                downloadSpeed += bytesRead;
+                if (downloadSpeed >= speed) {
+                    long desiredTime = 1024 / speed;
+                    long outputTime = 1024 / downloadSpeed;
+                    Thread.sleep(1000 * (desiredTime / outputTime));
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Incorrect number of arguments");
+        }
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        String file = args[2];
+        Thread wget = new Thread(new Wget(url, speed, file));
+        wget.start();
+        wget.join();
+    }
+}
