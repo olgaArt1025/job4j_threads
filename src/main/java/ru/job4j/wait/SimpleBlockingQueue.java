81fb2c16511c386ee1adb5fb863333ccf3a3a447
Olga_ <olga_art1025@mail.ru>
2022-03-13 00:34:12 +0500

    1. Реализовать шаблон Producer Consumer. [#1098]

---
+++ src/main/java/ru/job4j/wait/SimpleBlockingQueue.java
@@ -0,0 +1,39 @@
+package ru.job4j.wait;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+    @GuardedBy("this")
+    private final Queue<T> queue = new LinkedList<>();
+    private final int limit;
+
+    public SimpleBlockingQueue(int limit) {
+        this.limit = limit;
+    }
+
+    public synchronized void offer(T value) throws InterruptedException {
+        while (this.queue.size() == this.limit) {
+            wait();
+        }
+        queue.add(value);
+        notifyAll();
+    }
+
+    public synchronized T poll() throws InterruptedException {
+        while (queue.isEmpty()) {
+            queue.wait();
+        }
+        T rsl = queue.poll();
+        notifyAll();
+        return rsl;
+    }
+
+    public synchronized int size() {
+        return queue.size();
+    }
+}
---
+++ src/test/java/ru/job4j/wait/SimpleBlockingQueueTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.wait;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class SimpleBlockingQueueTest {
+
+    @Test
+    public void whenConsumerProducer() throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(12);
+        Thread producer = new Thread(() -> {
+            for (int i = 0; i < 12; i++) {
+                try {
+                    queue.offer(i);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+        Thread consumer = new Thread(() -> {
+            for (int i = 0; i < 6; i++) {
+                try {
+                    queue.poll();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.join();
+        assertThat(queue.poll(), equalTo(6));
+    }
+
+    @Test
+    public void whenProducerEqualToFive() throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(12);
+        Thread producer = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                try {
+                    queue.offer(i);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+
+        producer.start();
+        producer.join();
+        assertThat(queue.size(), is(5));
+    }
+}
